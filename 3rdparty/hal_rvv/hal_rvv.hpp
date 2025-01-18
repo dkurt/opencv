1717cb770a7e60f882e6c42fd7551445a56c0075
amane-ame <haofen.opencv@isrc.iscas.ac.cn>
2025-01-19 02:39:21 +0800

    Add HAL RVV convertScale, for Mat.convertTo.
    Co-authored-by: Liutong HAN <liutong2020@iscas.ac.cn>

--- 3rdparty/hal_rvv/hal_rvv.hpp
+++ 3rdparty/hal_rvv/hal_rvv.hpp
@@ -25,7 +25,8 @@
 #include "hal_rvv_1p0/minmax.hpp" // core
 #include "hal_rvv_1p0/atan.hpp" // core
 #include "hal_rvv_1p0/norm.hpp" // core
-#include "hal_rvv_1p0/normdiff.hpp" // core
+#include "hal_rvv_1p0/norm_diff.hpp" // core
+#include "hal_rvv_1p0/convert_scale.hpp" // core
 #endif
 
 #endif
