75fe389b117f87b6e3b04b2e7580e361f6192826
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2025-02-01 18:29:33 +0300

    Run baseline

--- platforms/linux/riscv64-gcc.toolchain.cmake
+++ platforms/linux/riscv64-gcc.toolchain.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
-set(GNU_MACHINE riscv64-linux-gnu CACHE STRING "GNU compiler triple")
+set(GNU_MACHINE riscv64-unknown-linux-gnu CACHE STRING "GNU compiler triple")
 
 include("${CMAKE_CURRENT_LIST_DIR}/flags-riscv64.cmake")
 if(COMMAND ocv_set_platform_flags)
