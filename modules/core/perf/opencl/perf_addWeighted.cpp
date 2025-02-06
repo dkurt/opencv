6f4cd4e2df5948abe985665bf0847b6ef4c9dce9
student15 <student15@master.nngu>
2025-02-06 14:32:24 +0300

    add ifndef & perftest

--- modules/core/perf/opencl/perf_addWeighted.cpp
+++ modules/core/perf/opencl/perf_addWeighted.cpp
@@ -1,7 +1,6 @@
 #include "perf_precomp.hpp"
 
-namespace opencv_test
-{
+namespace cv { namespace cv_hal_rvv {
 using namespace perf;
 
 #define TYPICAL_MAT_TYPES_ADWEIGHTED  CV_8UC1, CV_8UC4, CV_8SC1, CV_16UC1, CV_16SC1, CV_32SC1
@@ -34,4 +33,33 @@ PERF_TEST_P(Size_MatType, addWeighted, TYPICAL_MATS_ADWEIGHTED)
     SANITY_CHECK(dst, depth == CV_32S ? 4 : 1);
 }
 
+PERF_TEST_P(Size_MatType, AddWeighted_RVV, TYPICAL_MATS_ADWEIGHTED)
+{
+    Size size = get<0>(GetParam());
+    int type = get<1>(GetParam());
+
+    Mat src1(size, type);
+    Mat src2(size, type);
+    Mat dst(size, type);
+    double alpha = 0.75f;
+    double beta = 0.25f;
+    double gamma = 50.0f;
+
+    declare.in(src1, src2, WARMUP_RNG).out(dst);
+    // Выполнение тестового цикла
+    TEST_CYCLE()
+    {
+        cv::cv_hal_rvv::addWeighted8u(
+            src1.ptr<uchar>(), static_cast<size_t>(src1.step),
+            src2.ptr<uchar>(), static_cast<size_t>(src2.step),
+            dst.ptr<uchar>(), static_cast<size_t>(dst.step),
+            size.width, size.height,
+            &alpha, &beta, &gamma
+        );
+    }
+
+    // Проверка корректности результата
+    SANITY_CHECK(dst, 1);
+}
+};
 } // namespace
